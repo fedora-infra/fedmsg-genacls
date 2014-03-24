aa4facc69ad1b5c3a95f3de92ff50007c980df1c
Ralph Bean <rbean@redhat.com>
2014-03-24 12:20:28 -0400

    0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='fedmsg_genacls',
-    version='0.0.1',
+    version='0.1',
     description='A fedmsg consumer that sets gitosis acls in response to pkgdb messages',
     license="LGPLv2+",
     author='Janez Nemanič and Ralph Bean',
